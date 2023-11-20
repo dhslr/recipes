e0ac021fde44e3ca00a0e2be2b516c67d36672a3
Daniel Hassler <daniel.hassler@gmail.com>
2023-11-20 07:52:15 +0100

    Disable registration in PROD

--- lib/recipes.ex
+++ lib/recipes.ex
@@ -6,4 +6,22 @@ defmodule Recipes do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @doc """
+  Looks up `Application` config or raises if keyspace is not configured.
+
+  ## Examples
+      config :recipes, registration_enabled: true
+      iex> Recipes.config([:recipes, :registration_enabled])
+  """
+  def config([main_key | rest] = keyspace) when is_list(keyspace) do
+    main = Application.fetch_env!(:recipes, main_key)
+
+    Enum.reduce(rest, main, fn next_key, current ->
+      case Keyword.fetch(current, next_key) do
+        {:ok, val} -> val
+        :error -> raise ArgumentError, "no config found under #{inspect(keyspace)}"
+      end
+    end)
+  end
 end
