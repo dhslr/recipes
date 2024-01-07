41a6705d1b933ce6f1bea11da695b5cc720b398a
Daniel Hassler <daniel.hassler@gmail.com>
2024-01-07 14:25:58 +0100

    Increase version

--- backup.sh
+++ backup.sh
@@ -12,4 +12,4 @@ fi
 mkdir -p $BACKUP_DIR
 compose start db
 compose run --rm -e PGPASSWORD="${POSTGRES_PASSWORD}" db /usr/bin/pg_dump -h db --no-owner -U $POSTGRES_USER $POSTGRES_DB --no-password > $BACKUP_DIR/recipes_db_dump_new.sql
-compose run --rm --entrypoint "" -v $BACKUP_DIR:/backup -u root app tar czpf /backup/photos_new.tgz /app/lib/recipes-0.1.3/priv/static/photos
+compose run --rm --entrypoint "" -v $BACKUP_DIR:/backup -u root app tar czpf /backup/photos_new.tgz /app/lib/recipes-0.2.0/priv/static/photos
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Recipes.MixProject do
   def project do
     [
       app: :recipes,
-      version: "0.1.3",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
