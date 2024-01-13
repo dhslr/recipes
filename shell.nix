249e334178e5667b3dc2be1304857f73b1b9790f
Daniel Hassler <daniel.hassler@gmail.com>
2024-01-13 10:45:15 +0100

    Improve nix shell file

--- shell.nix
+++ shell.nix
@@ -1,9 +1,7 @@
- let
-   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.05";
-   pkgs = import nixpkgs { config = {}; overlays = []; };
- in
+{ pkgs ? import <nixpkgs> {} }:
 
- pkgs.mkShell {
+pkgs.mkShell {
+   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
    packages = with pkgs; [
      git
      nodejs
@@ -11,10 +9,13 @@
      gh
      postgresql
    ];
-
-   GIT_EDITOR = "${pkgs.neovim}/bin/nvim";
-
    shellHook = ''
-    git status
+     tmpdir=$(mktemp -d)
+     initdb -D $tmpdir
+     pg_ctl -D $tmpdir -l logfile -o "--unix_socket_directories='$PWD'" start
+     createuser -h $(pwd) --createdb postgres
+     createdb -h $(pwd) -U postgres recipes_dev
+     psql -h $(pwd) -U postgres recipes_dev < ./priv/repo/seeds.sql
+     trap 'pg_ctl -D $tmpdir -o "--unix_socket_directories=$PWD" stop' EXIT
    '';
- }
+} 
