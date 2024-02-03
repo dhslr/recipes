7e21e2145a9579e2b6ab36fc91ec6924b95bf840
Daniel Hassler <daniel.hassler@gmail.com>
2024-02-03 11:34:16 +0100

    Update shell nix

--- shell.nix
+++ shell.nix
@@ -5,17 +5,17 @@ pkgs.mkShell {
    packages = with pkgs; [
      git
      nodejs
-     elixir_1_15 
+     elixir_1_15
      gh
      postgresql
    ];
    shellHook = ''
      tmpdir=$(mktemp -d)
      initdb -D $tmpdir
-     pg_ctl -D $tmpdir -l logfile -o "--unix_socket_directories='$PWD'" start
+     pg_ctl -D $tmpdir -l $tmpdir/log -o "--unix_socket_directories='$tmpdir'" start
      createuser -h $(pwd) --createdb postgres
      createdb -h $(pwd) -U postgres recipes_dev
      psql -h $(pwd) -U postgres recipes_dev < ./priv/repo/seeds.sql
      trap 'pg_ctl -D $tmpdir -o "--unix_socket_directories=$PWD" stop' EXIT
    '';
-} 
+}
