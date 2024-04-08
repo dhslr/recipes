57d3ecf27e85010b5f5e85e41890dc9ab5f21a82
Daniel H <daniel@dhslr.de>
2024-04-08 07:17:31 +0200

    Add manifest.json
    Update favicon and manifest.json
    
    Improve manifest

--- lib/recipes_web.ex
+++ lib/recipes_web.ex
@@ -17,7 +17,7 @@ defmodule RecipesWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt photos)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt photos manifest.json)
 
   def router do
     quote do
