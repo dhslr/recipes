3ee91f6647c6aa5d9fcebd051cec5dd4561aad4a
Daniel Hassler <daniel@dhslr.de>
2024-10-13 21:07:11 +0200

    Improve test by using fixtures

--- test/recipes_web/live/user_login_feature_test.exs
+++ test/recipes_web/live/user_login_feature_test.exs
@@ -1,20 +1,22 @@
 defmodule RecipesWeb.RecipeEditUiTest do
   use ExUnit.Case, async: true
   use Wallaby.Feature
+  import Recipes.AccountsFixtures
 
-  @user_email "user@dhslr.de"
-  @user_password "VeryVeryS3cret!"
+  @password "VeryVeryS3cret!"
 
   setup do
-    Recipes.Accounts.register_user(%{email: @user_email, password: @user_password})
-    :ok
+    %{user: user_fixture(%{password: @password})}
   end
 
-  feature "a registered user can login and is redirected to recipes overview", %{session: session} do
+  feature "a registered user can login and is redirected to recipes overview", %{
+    session: session,
+    user: user
+  } do
     session
     |> visit("/recipes")
-    |> fill_in(Query.text_field("user_email"), with: @user_email)
-    |> fill_in(Query.text_field("user_password"), with: @user_password)
+    |> fill_in(Query.text_field("user_email"), with: user.email)
+    |> fill_in(Query.text_field("user_password"), with: @password)
     |> click(Query.button("Log in"))
     |> assert_has(Query.text_field("Search"))
   end
