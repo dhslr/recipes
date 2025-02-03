09686d6a0c9643474268c0f605708f2ed347ce97
daniel <daniel@dhslr.de>
2025-02-03 07:53:20 +0100

    flake based devenv

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [{
+                  languages.javascript.enable = true;
+                  languages.javascript.npm.enable = true;
+                  services.postgres = {
+                    enable = true;
+                    package = pkgs.postgresql_15;
+                    initialScript = ''
+                      CREATE ROLE postgres WITH CREATEDB LOGIN PASSWORD 'postgres';
+                    '';
+                  };
+                  packages = [
+                      pkgs.chromedriver
+                      pkgs.elixir
+                      pkgs.erlang
+                  ];
+                }
+              ];
+            };
+          });
+    };
+}
