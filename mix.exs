63b3ef74b10db3f3dbd9b661bb478b87297af996
Daniel Hassler <daniel@dhslr.de>
2025-12-16 06:47:23 +0100

    inc version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Recipes.MixProject do
   def project do
     [
       app: :recipes,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
