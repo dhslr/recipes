11b395140b46b0c296cb1811bd781181db6fc6c8
Daniel Hassler <daniel@dhslr.de>
2025-12-16 22:00:19 +0100

    Override host for ci

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :bcrypt_elixir, :log_rounds, 1
 config :recipes, Recipes.Repo,
   username: "recipes",
   password: "recipes",
-  hostname: "postgresql",
+  hostname: System.get_env("POSTGRES_HOST") || "postgresql",
   database: "recipes_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 20
